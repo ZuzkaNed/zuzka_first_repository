925f0f9b6b4263e694ac2436e52fb8b30049036e
zuzka <zuzka@nedved.org>
2023-10-19 11:50:09 +0200

    Lesson 3, tasks for SUM

--- lekce_3.sql
+++ lekce_3.sql
@@ -36,3 +36,50 @@ ORDER BY year_of_entry, category_code;
 SELECT 
 *
 FROM czechia_price;
+
+SELECT 
+	sum(value) AS value_sum
+FROM czechia_payroll
+WHERE value_type_code = '316';
+
+SELECT 
+	czechia_price.category_code,
+	sum(value) AS sum_value
+FROM czechia_price
+WHERE region_code = 'CZ064'
+GROUP BY category_code; 
+
+SELECT 
+*
+FROM czechia_price_category;
+
+
+SELECT 
+*
+FROM czechia_region;
+
+SELECT
+		sum(value) AS sum_value
+FROM czechia_price
+WHERE date_from = '2018-01-15';
+
+SELECT
+    SUM(value) AS sum_of_average_prices
+FROM czechia_price
+WHERE date_from = CAST('2018-01-15' AS date);
+
+SELECT 
+	category_code,
+	count(*) AS row_count,
+	sum(value) AS sum_value
+FROM czechia_price
+WHERE YEAR(date_from) = '2018'
+GROUP BY category_code;
+
+
+
+
+
+
+
+
