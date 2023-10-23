21ea7f7669e315891405a836e8290756805368b6
zuzka <zuzka@nedved.org>
2023-10-23 15:39:35 +0200

    project 1 file created

---
+++ projekt1.sql
@@ -0,0 +1,33 @@
+SELECT 
+	*	
+FROM czechia_payroll
+WHERE value_type_code = 5958 AND industry_branch_code = 'a' AND payroll_year = 2000 ;
+
+
+SELECT 
+	payroll_year,
+	industry_branch_code,
+	avg(value) AS avg_payroll_per_year
+FROM czechia_payroll
+WHERE value_type_code = 5958 AND industry_branch_code = 'a' AND calculation_code = '100'
+GROUP BY payroll_year;
+
+SELECT 
+*
+FROM czechia_payroll_value_type ;
+
+SELECT 
+*
+FROM czechia_payroll_unit;
+
+SELECT
+*
+FROM czechia_payroll_industry_branch;
+
+SELECT 
+*
+FROM czechia_payroll_calculation;
+
+
+
+
